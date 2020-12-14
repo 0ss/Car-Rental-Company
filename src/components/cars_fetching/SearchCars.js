98d9cef28bfc564b9da898ee071ebad59f1fb592
Hady <hadysata@gmail.com>
2020-12-14 16:30:57 +0300

    Implement search and searchcars

--- src/components/cars_fetching/SearchCars.js
+++ src/components/cars_fetching/SearchCars.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Link } from 'react-router-dom'
 import Navbar from '../../layout/Navbar'
 import Car from '../../images/Dodge-Ram-2015.jpg'
@@ -7,78 +7,203 @@ import '../../styles/main_styles.css'
 import SearchItems from './SearchBar'
 import Footer from '../../layout/Footer'
 import SearchCarsUI from './SearchCarsUI'
+import * as Firestore from '../../services/api/firestore'
+
+
+
+
+function getSearchOptions() {
+    const hash = window.location.hash
+
+
+    var searchOptions = {};
+
+    searchOptions.color = getHashItem(hash, "color")
+    searchOptions.size = getHashItem(hash, "size")
+    searchOptions.model = getHashItem(hash, "model")
+    searchOptions.priceFrom = getHashItem(hash, "priceFrom")
+    searchOptions.priceTo = getHashItem(hash, "priceTo")
+
+    return searchOptions;
+}
+
+function getHashItem(hash, type) {
+    return hash?.split(type + "=")[1]?.split("&")[0];
+}
+
+function filterCars(cars) {
+    const options = getSearchOptions();
+    var filteredCars = [];
+
+    const isColorMatch = ((car) => car.color?.toLowerCase() === options.color?.toLowerCase());
+    const isModelMatch = ((car) => car.model?.toLowerCase() === options.model?.toLowerCase());
+    const isSizeMatch = ((car) => car.size?.toLowerCase() === options.size?.toLowerCase());
+    const isPriceMatch = ((car) => car.price >= options.priceFrom && car.price <= options.priceTo);
+
+
+    if (options.color && options.model && options.size && options.priceTo && options.priceFrom) {
+        for (const car of cars) {
+            if (isColorMatch(car) && isModelMatch(car) && isSizeMatch(car) && isPriceMatch(car)) {
+                filteredCars.push(car);
+            }
+
+        }
+    } else if (options.color && options.model && options.size) {
+        for (const car of cars) {
+            if (isColorMatch(car) && isModelMatch(car) && isSizeMatch(car)) {
+                filteredCars.push(car);
+            }
+
+        }
+    } else if (options.color && options.model) {
+        for (const car of cars) {
+            if (isColorMatch(car) && isModelMatch(car)) {
+                filteredCars.push(car);
+            }
+
+        }
+    } else if (options.size && options.model) {
+        for (const car of cars) {
+            if (isSizeMatch(car) && isModelMatch(car)) {
+                filteredCars.push(car);
+            }
+
+        }
+    } else if (options.color && options.size) {
+        for (const car of cars) {
+            if (isColorMatch(car) && isSizeMatch(car)) {
+                filteredCars.push(car);
+            }
+
+        }
+    } else if (options.color && options.priceFrom && options.priceTo) {
+        for (const car of cars) {
+            if (isColorMatch(car) && isPriceMatch(car)) {
+                filteredCars.push(car);
+            }
+
+        }
+    }
+    else if (options.model && options.priceFrom && options.priceTo) {
+        for (const car of cars) {
+            if (isModelMatch(car) && isPriceMatch(car)) {
+                filteredCars.push(car);
+            }
+        }
+    } else if (options.size && options.priceFrom && options.priceTo) {
+        for (const car of cars) {
+            if (isSizeMatch(car) && isPriceMatch(car)) {
+                filteredCars.push(car);
+            }
+
+        }
+    } else if (options.color) {
+        for (const car of cars) {
+            if (isColorMatch(car)) {
+                filteredCars.push(car);
+            }
+
+        }
+    } else if (options.model) {
+        for (const car of cars) {
+            if (isModelMatch(car)) {
+                filteredCars.push(car);
+            }
+
+        }
+    } else if (options.size) {
+        for (const car of cars) {
+            if (isSizeMatch(car)) {
+                filteredCars.push(car);
+            }
+
+        }
+    } else if (options.priceFrom && options.priceTo) {
+        for (const car of cars) {
+            if (isPriceMatch(car)) {
+                filteredCars.push(car);
+            }
+
+        }
+    } else {
+        filteredCars = cars;
+    }
+
+    return [...new Set(filteredCars)];
+
+
+}
+
 export default function SearchCars() {
+    const [cars, setCars] = useState(null);
+    const [shownCars, setShownCars] = useState(null);
+    const [tempHash , setTempHash] = useState(null);
 
-    
-    const cars  = [
-        {
-            id:'1',
-            name : 1,
-            price: "385.85",
-            imageSrc : Car
-        },
-        {
-            id:'2',
-            name : 2,
-            price: "385.85",
-            imageSrc : Car
-        },
-        {
-            id:'3',
-            name : 2,
-            price: "385.85",
-            imageSrc : Car
-        },
-    ]
-    /*
-    above you should retreive all the object from the firebase and
-    put them in cars array, then as you can see below, it dynamcilly going to take each object
-    and put it on a column, and the whole array then is going to be passed to SearchCarsUI
-    to add row, you dont really need to worry about this. just retrieve the object from firebase <3
+    function getCars() {
+        Firestore.getCars().then((result) => {
+            console.log(result)
+            if (result.status === "ok") {
+                setCars(result.result)
+                setShownCars(filterCars(result.result))
+            }
+        })
+    }
 
-    */
+    function checkHash(){
+        if(tempHash !== window.location.hash){
+            setTempHash(window.location.hash);
+            if(!cars)
+            getCars();
+            else{
+                setShownCars(filterCars(cars))
+            }
+            console.log("change in hash!")
+        }
+    }
 
 
-    const carsArray = cars.length ? cars.map(car=>{
-        return(
-        <div className="col-md-4 mb-4">
-            <div class="card">
-                <img class="card-img-top car-img" src={car.imageSrc} alt="Card cap"></img>
-                <div class="card-body" >
-                    <h5 class="card-title text-center">
-                        <span className="car-color float-right">
-                        </span>
-                        {car.name}
+    checkHash()
+
+    const carsArray = shownCars?.length ? shownCars.map(car => {
+        return (
+            <div className="col-md-4 mb-4">
+                <div class="card">
+                    <img class="card-img-top car-img" src={car.image} alt="Card cap"></img>
+                    <div class="card-body" >
+                        <h5 class="card-title text-center">
+                            <span className="car-color float-right">
+                            </span>
+                            {car.name}
+                        </h5>
+                        <h5 class="car-price mb-3 mt-3 text-center">
+                            {car.price}$
                     </h5>
-                    <h5 class="car-price mb-3 mt-3 text-center">
-                        {car.price}
-                    </h5>
-                    <Link to={`/viewcar#${car.id}`}>
-                        <button className="btn float-right">
-                            <span className="font-weight-bold">Buy now!</span>
-                        </button>
-                    </Link>
+                        <Link to={`/viewcar?id=${car.id}`}>
+                            <button className="btn float-right">
+                                <span className="font-weight-bold">Buy now!</span>
+                            </button>
+                        </Link>
+                    </div>
                 </div>
             </div>
-        </div>
         )
     }) : null
 
     return (
         <>
             <Navbar />
-                <div className="container mt-3">
-                    <SearchItems />
+            <div className="container mt-3">
+                <SearchItems />
 
-                    {
-                        carsArray 
-                        ? 
-                        <SearchCarsUI  cars={carsArray} /> 
+                {
+                    carsArray
+                        ?
+                        <SearchCarsUI cars={carsArray} />
                         :
                         <h1 className="text-center">Cars inventory is empty</h1>
-                    }
+                }
 
-                </div>
+            </div>
             <Footer />
         </>
     )
--- src/services/api/firestore.js
+++ src/services/api/firestore.js
@@ -4,46 +4,54 @@ import uuid from 'react-uuid'
 
 export async function set(collection, doc, data) {
     return await Api.Post('firestore/set', { collection: collection, doc: doc, data: JSON.stringify(data) });
-  
+
 }
 
 export async function get(collection, doc) {
     return await Api.Post('firestore/get', { collection: collection, doc: doc, uid: Auth.getUid() });
 }
 
-export async function addCar(name, color , model , size , status , location, price , image , id) {
-    if(!id) id = uuid()
-    if(await Auth.checkUid(Auth.getUid())){
-   return await set('cars' , id , {name:name, color:color, model:model , size:size,status:status,location:location,price:price , image:image , id:id})
-    }else{
+export async function getList(collection) {
+    return await Api.Post('firestore/list/get', { collection: collection, uid: Auth.getUid() });
+}
+
+export async function addCar(name, color, model, size, status, location, price, image, id) {
+    if (!id) id = uuid()
+    if (await Auth.checkUid(Auth.getUid())) {
+        return await set('cars', id, { name: name, color: color, model: model, size: size, status: status, location: location, price: price, image: image, id: id })
+    } else {
         return false;
     }
 }
 
 export async function getCar(carId) {
-   return await get('cars' , carId)
+    return await get('cars', carId)
 }
 
-export async function addOrder(orderId, carId , price , days , dateFrom , dateTo , paymentMethod) {
-    if(await Auth.checkUid(Auth.getUid())){
-   return await set('orders' , orderId , {id:orderId, uid:Auth.getUid() , carId:carId , price: price , days:days , dateFrom:dateFrom , dateTo:dateTo , paymentMethod:paymentMethod })
-    }else{
+export async function getCars() {
+    return await getList('cars')
+}
+
+export async function addOrder(orderId, carId, price, days, dateFrom, dateTo, paymentMethod) {
+    if (await Auth.checkUid(Auth.getUid())) {
+        return await set('orders', orderId, { id: orderId, uid: Auth.getUid(), carId: carId, price: price, days: days, dateFrom: dateFrom, dateTo: dateTo, paymentMethod: paymentMethod })
+    } else {
         return false;
     }
 }
 
 export async function getOrder(orderId) {
-    return await get('orders' , orderId)
- }
+    return await get('orders', orderId)
+}
 
-export async function uploadImage(path , fileName , fileType , base64Data) {
-    if(await Auth.checkUid(Auth.getUid())){
-   return await Api.Post('storage/set' , {path:path , fileName:fileName , fileType:fileType, base64Data:base64Data})
-    }else{
+export async function uploadImage(path, fileName, fileType, base64Data) {
+    if (await Auth.checkUid(Auth.getUid())) {
+        return await Api.Post('storage/set', { path: path, fileName: fileName, fileType: fileType, base64Data: base64Data })
+    } else {
         return false;
     }
 }
 
-export function getUuid(){
+export function getUuid() {
     return uuid();
 }
